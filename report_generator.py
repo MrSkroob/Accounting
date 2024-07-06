747aea17dc9d1a36ba451f61d6cba391f6c433b3
Dr Yaccob <yaccoby101@gmail.com>
2024-07-06 16:44:39 +0100

    Cleaning up imports

--- report_generator.py
+++ report_generator.py
@@ -1,6 +1,5 @@
 # from datetime import date
 from datatypes import *
-import database
 
 
 def singular_report(period_entry: PeriodEntry, report_start: date, report_end: date) -> SimplifiedReport:
@@ -113,29 +112,3 @@ def display_report(report: SimplifiedReport):
     print("Customer advances:", report.supplier_advances)
     print("Profit in report period: ", report.loss_in_period)
     print("Profit prior to report period:", report.prior_loss_in_period)
-
-
-# entries = [
-#     PeriodEntry(date(2024, 1, 1), date(2024, 2, 1),
-#                 [Payment(date(2024, 1, 1), 1000),
-#                  Payment(date(2024, 1, 15), 1000)],
-#                 "Any"),
-#     PeriodEntry(date(2024, 1, 1), date(2024, 3, 1),
-#                 [Payment(date(2024, 3, 1), 500),
-#                  Payment(date(2024, 3, 15), 500)],
-#                 "Any"),
-#     PeriodEntry(date(2024, 1, 1), date(2024, 4, 1),
-#                 [Payment(date(2024, 4, 1), 500),
-#                  Payment(date(2024, 4, 15), 500)],
-#                 "Any")
-# ]
-
-if __name__ == "__main__":
-    entries = database.get_period_entries()
-    my_report = generate_report(period_entries=entries,
-                                report_start=date(2024, 1, 1),
-                                report_end=date(2024, 5, 31))
-    display_report(my_report)
-
-    # for i in entries:
-    #     database.add_period_entry(i)
