0cdc7c2621ee00cbe248214f1c42adb4a7b589e5
Dr Yaccob <yaccoby101@gmail.com>
2024-07-09 17:12:38 +0100

    converted two lists "PAYMENT_TYPES" and "PAYMENT_HEADERS" into a single dictionary.

--- Utils/init_database.py
+++ Utils/init_database.py
@@ -12,19 +12,16 @@ PERIODS_TABLE = ("CREATE TABLE Periods(PeriodID INTEGER PRIMARY KEY, "
                  "Calculation REAL, "
                  "IsForecast INTEGER)")
 
-PERIODS_HEADER = ["PeriodID", "DateCreated", "StartDate", "EndDate",
-                  "Category", "ForecastRule", "Calculation", "IsForecast"]
-
-PERIODS_TYPES = [
-    "INTEGER",
-    "TEXT",
-    "TEXT",
-    "TEXT",
-    "TEXT",
-    "TEXT",
-    "REAL",
-    "INTEGER"
-]
+PERIODS_TYPES = {
+    "PeriodID": "INTEGER",
+    "DateCreated": "TEXT",
+    "StartDate": "TEXT",
+    "EndDate": "TEXT",
+    "Category": "TEXT",
+    "ForecastRule": "TEXT",
+    "Calculation": "REAL",
+    "IsForecast": "INTEGER"
+}
 
 PAYMENTS_TABLE = ("CREATE TABLE Payments("
                   "PaymentID TEXT PRIMARY KEY, "
@@ -34,15 +31,13 @@ PAYMENTS_TABLE = ("CREATE TABLE Payments("
                   "Client TEXT,"
                   "FOREIGN KEY(PeriodID) REFERENCES Periods(PeriodID))")
 
-PAYMENTS_HEADER = ["PaymentID", "PeriodID", "PayDate", "Net", "Client"]
-
-PAYMENTS_TYPES = [
-    "TEXT",
-    "INTEGER",
-    "TEXT",
-    "REAL",
-    "TEXT"
-]
+PAYMENTS_TYPES = {
+    "PaymentID": "TEXT",
+    "PeriodID": "INTEGER",
+    "PayDate": "TEXT",
+    "Net": "REAL",
+    "Client": "TEXT"
+}
 
 if __name__ == "__main__":
     connection = sqlite3.connect("../PE.db")
